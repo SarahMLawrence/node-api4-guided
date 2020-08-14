1633196a3a24bcff1026acc4e984023c98ce261e
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-08-13 21:14:33 -0400

    deploying

--- shouts/shouts-router.js
+++ shouts/shouts-router.js
@@ -1,13 +1,20 @@
-const express = require("express")
-const shoutsModel = require("./shouts-model")
+const express = require("express");
+const shoutsModel = require("./shouts-model");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	shoutsModel.find()
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await shoutsModel.find();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+
+  // shoutsModel.find()
+  // 	.then((data) => res.status(200).json(data))
+  // 	.catch((err) => next(err))
+});
 
 router.get("/:id", validateShoutId(), (req, res, next) => {
 	res.status(200).json(req.shout)
